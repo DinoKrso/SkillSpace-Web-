77d39ec0ec78c3656ee9a09aa92d6bc87aca5267
Dino Krso <dino.krso45@hotmail.com>
2023-06-29 16:36:59 +0200

    Middleware

--- rest/index.php
+++ rest/index.php
@@ -5,6 +5,8 @@ require '../vendor/autoload.php';
 require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/CourseService.php';
 require_once "dao/UserDao.class.php" ;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 
 Flight::register('userService', "UserService");
@@ -23,6 +25,28 @@ require_once __DIR__ . '/routes/CourseRoutes.php';
 Flight::route('GET /', function () {
     echo "Hello";
 });
+
+ // middleware method for login
+Flight::route('/*', function(){
+    //perform JWT decode
+    $path = Flight::request()->url;
+    if ($path == '/login' || $path == '/docs.json') return TRUE; // exclude login route from middleware
+  
+    $headers = getallheaders();
+    if (!$headers['Authorization']){
+      Flight::json(["message" => "Authorization is missing"], 403);
+      return FALSE;
+    }else{
+      try {
+        $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+        Flight::set('user', $decoded);
+        return TRUE;
+      } catch (\Exception $e) {
+        Flight::json(["message" => "Authorization token is not valid"], 403);
+        return FALSE;
+      }
+    }
+  });
 
 Flight::start();
 
